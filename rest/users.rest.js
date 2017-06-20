782af72ad4bae237425f686c871cf4f6a496953e
markrudenkov <mark.rudenkov@gmail.com>
2017-06-20 15:10:22 +0300

    created user registration

--- rest/users.rest.js
+++ rest/users.rest.js
@@ -47,4 +47,34 @@ module.exports = function (router, User) {
                 }
             });
         });
+
+    router.route('/users/registration')
+        .post(function (req, res) {
+            User.findOne({ email: req.body.email}, function (err,user) {
+                if (!user) {
+                    let user = new User({
+                        name: req.body.name,
+                        surname: req.body.surname,
+                        password: req.body.password,
+                        email: req.body.email,
+                        created_at: req.body.created_at,
+                        updated_at: req.body.updated_at
+                    });
+                    // save the user and check for errors
+                    user.save(function (err) {
+                        if (err) {
+                            console.log('ERROR CREATING USER: ' + err);
+                            res.status(500).json({error: err});
+                        } else {
+                            console.log('SUCCESS CREATING USER: ' + user.name);
+                            res.status(200).json({message: 'User created!'});
+                        }
+                    });
+                } else {
+                    console.log('User with this email eqists'.green);
+                    // res.status(200).json(user);
+                    res.status(200).json({"message" : "User with this email eqists"});
+                }
+            });
+        });
 };
