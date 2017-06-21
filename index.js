7496cbbaf5cfd1c39f2f0ed0f01781cec6290d73
laurasud <laurasudeikyte@gmail.com>
2017-06-21 10:50:14 +0300

    Small fix for data posting

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 // allow cross domain connection
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
     next();
 });
 
