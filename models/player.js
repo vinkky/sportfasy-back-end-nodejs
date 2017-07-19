9d85e7fe4eb96153d1fcb0b739a595092216ea4b
laurasud <laurasudeikyte@gmail.com>
2017-07-19 18:00:56 +0300

    fetching data, adding to db, GET method

--- models/player.js
+++ models/player.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-var fetch = require('node-fetch');
+let fetch = require('node-fetch');
 
 let playerSchema = new Schema({
     name: {type: String},
@@ -8,13 +8,14 @@ let playerSchema = new Schema({
     age: {type: Number},
     nationality: {type: String},
     real_team: {type: String},
-    fantasy_team: {type: String},
     eff_points: {type: Number},
     price: {type: Number}
 });
 
 //create model for player
 let Player = mongoose.model('Player', playerSchema);
+
+//API url
 let url = 'http://ergast.com/api/f1/current/last/results.json';
 
 Player.count(function (err, count) {
@@ -24,12 +25,12 @@ Player.count(function (err, count) {
             .then((out) => {
                 let info = out.MRData.RaceTable.Races[0].Results;
                 info.forEach((item) => {
-                    obj = new Object();
-                    obj.name = item.Driver.givenName;
-                    obj.surname = item.Driver.familyName;
-                    obj.age = item.Driver.dateOfBirth;
-                    obj.nationality = item.Driver.nationality;
-                    obj.real_team = item.Constructor.constructorId;
+                    let obj = {};
+                    obj.name = item.Driver.givenName || 'No name';
+                    obj.surname = item.Driver.familyName || 'No surname';
+                    obj.age = item.Driver.dateOfBirth || 0;
+                    obj.nationality = item.Driver.nationality || 'No nationality';
+                    obj.real_team = item.Constructor.constructorId || 'No team';
 
                     Player.collection.insert(obj)
                 });
@@ -37,21 +38,6 @@ Player.count(function (err, count) {
             })
             .catch(err => console.error(err));
     }
-});
-
-// on every save, add the date
-playerSchema.pre('save', function (next) {
-    // get the current date
-    let currentDate = new Date();
-
-    // change the updated_at field to current date
-    this.updated_at = currentDate;
-
-    // if created_at doesn't exist, add to that field
-    if (!this.created_at)
-        this.created_at = currentDate;
-
-    next();
 });
 
 // export model
