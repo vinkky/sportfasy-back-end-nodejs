d14fdf6b148f88a8dd58bffc0157216a762ffbe8
unknown <stancikasdeividas@gmail.com>
2017-07-31 09:52:27 +0300

    fixed model update

--- models/race.js
+++ models/race.js
@@ -28,6 +28,9 @@ let Race = mongoose.model('Race', raceSchema);
 let url = 'http://ergast.com/api/f1/current/results.json';
 
 //If DB empty, fetch data
+
+Race.count(function (err, count) {
+    if (!err && count === 0) {
         fetch(url)
             .then(res => res.json())
             .then((out) => {
@@ -57,6 +60,8 @@ let url = 'http://ergast.com/api/f1/current/results.json';
 
             })
             .catch(err => console.error(err));
+    }
+});
 
 
 //Update data every 30 min
